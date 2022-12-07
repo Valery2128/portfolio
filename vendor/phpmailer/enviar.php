dd39ccafc5f7e49c898d4f7aefdc48e01034f666
Valery2128 <91564318+Valery2128@users.noreply.github.com>
2022-12-06 22:41:01 -0500

    cambios

--- vendor/phpmailer/enviar.php
+++ vendor/phpmailer/enviar.php
@@ -1,5 +1,8 @@
 <?php
-
+// use PHPMailer/PHPMailer/PHPMailer;
+if (isset($_POST['enviar'])) {
+    if(!empty($_POST (['nombre']) ));
+}
 $destino="gasaiyuno2808@gmail.com";
 $nombre=$_POST["nombre"];
 $telefono=$_POST["telefono"];
@@ -7,8 +10,14 @@ $correo=$_POST["correo"];
 $asunto=$_POST["asunto"];
 $mensaje=$_POST["mensaje"];
 $contenido= "Nombre: ".$nombre ."\nTelefono: " .$telefono . "\nCorreo:" .$correo ."\nAsunto:" .$asunto ."\nMensaje:" .$mensaje;
-mail($destino,$asunto,$contenido);
-header("Location:gracias.html");
+$header="Reply-To: gasaiyuno2808Qgmail.com" ."\r\n";
+$header= "X-Mailer: PHP/". phpversion();
+$mail= @mail ($destino,$asunto,$contenido);
+if($mail){
+echo header("Location:gracias.html");
+}
+
+
 
 
 ?>
